4742556e4e5973ab87a2e7f90fbfeaeaa0a4875d
Heromyth <bitworld@qq.com>
2019-05-20 17:47:22 +0800

    Add WildcardPermissionTest

--- examples/UnitTest/source/app.d
+++ examples/UnitTest/source/app.d
@@ -1,5 +1,7 @@
 import std.stdio;
 
+import test.shiro.authz.permission.AllPermissionTest;
+import test.shiro.authz.permission.WildcardPermissionTest;
 import test.shiro.subject.SimplePrincipalCollectionTest;
 import test.shiro.subject.DelegatingSubjectTest;
 import test.shiro.config.IniTest;
@@ -9,8 +11,11 @@ import hunt.util.UnitTest;
 
 void main()
 {
+	// authz.permission
+	// testUnits!(AllPermissionTest);
+	testUnits!(WildcardPermissionTest);
 
-	testUnits!(DelegatingSubjectTest);
+	// testUnits!(DelegatingSubjectTest);
 	// testUnits!(IniTest);
 	// testUnits!(SimplePrincipalCollectionTest);
 }

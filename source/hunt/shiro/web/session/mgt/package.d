a01e0d60f984084205a7de6ffe0ac1a3572564a1
heromyth <bitworld@qq.com>
2020-09-10 11:36:02 +0800

    Remove some redundancy modules.

--- source/hunt/shiro/web/session/mgt/package.d
+++
@@ -1,8 +0,0 @@
-module hunt.shiro.web.session.mgt;
-
-public import hunt.shiro.web.session.mgt.DefaultWebSessionContext;
-public import hunt.shiro.web.session.mgt.DefaultWebSessionManager;
-public import hunt.shiro.web.session.mgt.ServletContainerSessionManager;
-public import hunt.shiro.web.session.mgt.WebSessionContext;
-public import hunt.shiro.web.session.mgt.WebSessionKey;
-public import hunt.shiro.web.session.mgt.WebSessionManager;
